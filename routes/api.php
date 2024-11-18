5872e7070d1139fe8820ef3b67dd6639a456ed77
DeadPage7 <lolol.kek154@gmail.com>
2024-11-18 19:05:42 +0700

    обнова

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ClientController;
 
 // Регистрация
 Route::post('register', [AuthController::class, 'register']);
@@ -13,6 +14,14 @@ Route::post('login', [AuthController::class, 'login']);
 // Выход
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
+Route::get('client/{id}', [ClientController::class, 'show']);
+
 Route::middleware('auth:sanctum')->get('/client', function (Request $request) {
     return response()->json($request->user());
 });
+// Получить все товары
+Route::get('/product', [ProductController::class, 'index']); // Получить все товары
+// Получить товар по ID
+Route::get('/product/{id}', [ProductController::class, 'show']); // Получить товар по ID
+// Получение товаров по категории
+Route::get('/category/{id}/products', [CategoryController::class, 'product']);
