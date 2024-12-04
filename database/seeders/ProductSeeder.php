e1d24a0474605250d13dbca6cf5809eaca6aaea7
DeadPage7 <lolol.kek154@gmail.com>
2024-12-04 15:44:58 +0700

    переделан сидр продукта

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -15,7 +15,7 @@ class ProductSeeder extends Seeder
         Product::create([
             'name' => 'Альтерия',
             'price' => 300.00,
-            'photo' => 'public/products/image1.png',
+            'photo' => 'products/image1.png',
             'description' => 'Растение, которое станет идеальным дополнением любого пространства.',
             'category_id' => $categories[0]->id, // Модульные растения
         ]);
@@ -23,7 +23,7 @@ class ProductSeeder extends Seeder
         Product::create([
             'name' => 'Версия',
             'price' => 150.00,
-            'photo' => 'public/products/image2.png',
+            'photo' => 'products/image2.png',
             'description' => 'Подходит для тех, кто ценит минимализм и простоту.',
             'category_id' => $categories[3]->id, // Минималистичные формы
         ]);
@@ -31,7 +31,7 @@ class ProductSeeder extends Seeder
         Product::create([
             'name' => 'Синтексия',
             'price' => 200.00,
-            'photo' => 'public/products/image3.png',
+            'photo' => 'products/image3.png',
             'description' => 'Идеальный выбор для создания уютной атмосферы.',
             'category_id' => $categories[4]->id, // Декоративные решения
         ]);
@@ -39,7 +39,7 @@ class ProductSeeder extends Seeder
         Product::create([
             'name' => 'Эвокс',
             'price' => 100.00,
-            'photo' => 'public/products/image4.png',
+            'photo' => 'products/image4.png',
             'description' => 'Растение, которое радует своей неприхотливостью.',
             'category_id' => $categories[2]->id, // Универсальные культуры
         ]);
@@ -47,7 +47,7 @@ class ProductSeeder extends Seeder
         Product::create([
             'name' => 'Десфера',
             'price' => 350.00,
-            'photo' => 'public/products/image5.png',
+            'photo' => 'products/image5.png',
             'description' => 'Прекрасно подойдет для декорирования офиса или дома.',
             'category_id' => $categories[4]->id, // Декоративные решения
         ]);
@@ -55,7 +55,7 @@ class ProductSeeder extends Seeder
         Product::create([
             'name' => 'Флурия',
             'price' => 120.00,
-            'photo' => 'public/products/image6.png',
+            'photo' => 'products/image6.png',
             'description' => 'Легко выращивается и добавляет живости в интерьер.',
             'category_id' => $categories[0]->id, // Модульные растения
         ]);
@@ -63,7 +63,7 @@ class ProductSeeder extends Seeder
         Product::create([
             'name' => 'Гирофлора',
             'price' => 250.00,
-            'photo' => 'public/products/image7.png',
+            'photo' => 'products/image7.png',
             'description' => 'Универсальное растение для оформления различных пространств.',
             'category_id' => $categories[1]->id, // Инновационные цветы
         ]);
@@ -71,7 +71,7 @@ class ProductSeeder extends Seeder
         Product::create([
             'name' => 'Калидора',
             'price' => 400.00,
-            'photo' => 'public/products/image8.png',
+            'photo' => 'products/image8.png',
             'description' => 'Отличный выбор для тех, кто любит экспериментировать с растениями.',
             'category_id' => $categories[4]->id, // Декоративные решения
         ]);
@@ -79,7 +79,7 @@ class ProductSeeder extends Seeder
         Product::create([
             'name' => 'Элатия',
             'price' => 220.00,
-            'photo' => 'public/products/image9.png',
+            'photo' => 'products/image9.png',
             'description' => 'Универсальное растение с богатой историей.',
             'category_id' => $categories[0]->id, // Модульные растения
         ]);
@@ -87,7 +87,7 @@ class ProductSeeder extends Seeder
         Product::create([
             'name' => 'Норестия',
             'price' => 180.00,
-            'photo' => 'public/products/image10.png',
+            'photo' => 'products/image10.png',
             'description' => 'Хорошо вписывается в любую обстановку, добавляя уюта.',
             'category_id' => $categories[4]->id, // Декоративные решения
         ]);
